bdaa4734d70d0bccf01d05380b35750fd802ae11
Jing Li <thyrlian@gmail.com>
2013-11-03 09:14:21 +0100

    Added normalize method for letters like German umlaut

--- lib/listen_and_write/word.rb
+++ lib/listen_and_write/word.rb
@@ -1,14 +1,37 @@
+# encoding: UTF-8
+
 module ListenAndWrite
   class Word
-    attr_accessor :value, :translation
+    attr_accessor :value, :translation, :orthography
     
     def initialize(value, translation = nil)
       @value = value
       @translation = translation
+      @orthography = decompose(value)
     end
     
-    def decompose
-      @value.split(//)
+    def decompose(word)
+      word.split(//).map! do |x|
+        normalize(x)
+      end
     end
+    
+    def normalize(letter)
+      case letter.downcase.chomp
+      # German
+      when 'ä'
+        'ae'
+      when 'ö'
+        'oe'
+      when 'ü'
+        'ue'
+      when 'ß'
+        'ss'
+      else
+        letter
+      end
+    end
+    
+    private :decompose, :normalize
   end
 end
