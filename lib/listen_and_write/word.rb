7e4d1f94840842e012f0fd8f2b5e923ce8ad85e5
Jing Li <thyrlian@gmail.com>
2013-11-03 20:32:43 +0100

    Added serialization and deserialization for Word, and also corresponding tests

--- lib/listen_and_write/word.rb
+++ lib/listen_and_write/word.rb
@@ -1,13 +1,18 @@
 # encoding: UTF-8
 
+require 'json'
+
 module ListenAndWrite
   class Word
-    attr_accessor :value, :translation, :orthography
+    attr_accessor :value, :translation
     
     def initialize(value, translation = nil)
       @value = value
       @translation = translation
-      @orthography = decompose(value)
+    end
+    
+    def orthography
+      decompose(@value)
     end
     
     def decompose(word)
@@ -29,6 +34,19 @@ module ListenAndWrite
         'ss'
       else
         letter
+      end
+    end
+    
+    def to_json(*args)
+      {
+        'json_class' => self.class.name,
+        'data'       => [ @value, @translation ]
+      }.to_json(*args)
+    end
+    
+    class << self
+      def json_create(object)
+        new(*object['data'])
       end
     end
     
--- test/test_word.rb
+++ test/test_word.rb
@@ -5,7 +5,7 @@ require 'listen_and_write'
 
 class WordTest < Test::Unit::TestCase
   def setup
-    @word = ListenAndWrite::Word.new('Fußgängerübergänge')
+    @word = ListenAndWrite::Word.new('Fußgängerübergänge', '人行横道线')
   end
   
   def test_normalize_german
@@ -19,5 +19,20 @@ class WordTest < Test::Unit::TestCase
   
   def test_decompose_word_contains_german_umlaut
     assert_equal ['F','u','ss','g','ae','n','g','e','r','ue','b','e','r','g','ae','n','g','e'], @word.send(:decompose, 'Fußgängerübergänge')
+  end
+  
+  def test_orthography
+    assert_equal ['F','u','ss','g','ae','n','g','e','r','ue','b','e','r','g','ae','n','g','e'], @word.orthography
+  end
+  
+  def test_serialization
+    assert_equal '{"json_class":"ListenAndWrite::Word","data":["Fußgängerübergänge","人行横道线"]}', @word.to_json
+  end
+  
+  def test_deserialization
+    deserialized_object = JSON.parse(@word.to_json)
+    assert_equal @word.class, deserialized_object.class
+    assert_equal @word.value, deserialized_object.value
+    assert_equal @word.translation, deserialized_object.translation
   end
 end
