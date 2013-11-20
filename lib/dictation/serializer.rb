46618bcd7e666c785689ece3a70f37585c99ddef
Jing Li <thyrlian@gmail.com>
2013-11-20 22:36:29 +0100

    Added 2 deserialize methods for only a given portion

--- lib/dictation/serializer.rb
+++ lib/dictation/serializer.rb
@@ -17,6 +17,29 @@ module Dictation
         end
         words
       end
+
+      def deserialize_a_portion_by_given_word(file, start, stop = nil)
+        all_words = deserialize(file)
+        idx_start = all_words.index { |x| x.value == start }
+        idx_stop = ( stop.nil? ? nil : all_words.index { |x| x.value == stop } )
+        idx_stop = all_words.size if idx_stop.nil?
+        if !idx_start.nil? && idx_start <= idx_stop
+          return all_words[idx_start..idx_stop]
+        end
+      end
+
+      def deserialize_a_portion_by_given_line(file, start, stop = nil)
+        all_words = deserialize(file)
+        unless stop.nil?
+          if stop >= start && stop <= all_words.size
+            return all_words[start-1..stop-1]
+          end
+        else
+          if start <= all_words.size
+            return all_words[start-1..-1]
+          end
+        end
+      end
     end
   end
 end
