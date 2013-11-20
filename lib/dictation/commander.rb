c2b5e531d6e7df9f6614a2684c5508362883bcad
Jing Li <thyrlian@gmail.com>
2013-11-20 21:08:44 +0100

    Completed OptionParser

--- lib/dictation/commander.rb
+++ lib/dictation/commander.rb
@@ -2,59 +2,75 @@ require 'optparse'
 
 module Dictation
   class Commander
-    def self.order(args)
-      options = {}
-      commands = {
-          'new' => OptionParser.new do |opts|
-            opts.on('-d', '--dictate TTS', 'Specify two-letters language code for dictation') do |tts|
-              options[:dictate] = tts
-            end
+    class << self
+      def order(args)
+        options = {}
+        commands = {
+            'new' => OptionParser.new do |opts|
+              opts.banner = set_banner('new')
 
-            opts.on('-v', '--verify TTS', 'Specify two-letters language code for verification') do |tts|
-              options[:verify] = tts
-            end
-          end,
+              opts.on('-d', '--dictate TTS', 'Specify two-letters language code for dictation') do |tts|
+                options[:dictate] = tts
+              end
 
-          'dictate' => OptionParser.new do |opts|
-            opts.on('-b', '--begin WORD') do |word|
-              options[:dictate_begin] = word
-            end
+              opts.on('-v', '--verify TTS', 'Specify two-letters language code for verification') do |tts|
+                options[:verify] = tts
+              end
+            end,
 
-            opts.on('-e', '--end WORD') do |word|
-              options[:dictate_end] = word
-            end
+            'dictate' => OptionParser.new do |opts|
+              opts.banner = set_banner('dictate')
 
-            opts.on('-s', '--start LINE', Integer) do |line|
-              options[:dictate_start] = line
-            end
+              opts.on('-b', '--begin WORD', 'Begin with given word') do |word|
+                options[:dictate_begin] = word
+              end
 
-            opts.on('-f', '--finish LINE', Integer) do |line|
-              options[:dictate_finish] = line
-            end
-          end,
+              opts.on('-e', '--end WORD', 'End with given word') do |word|
+                options[:dictate_end] = word
+              end
 
-          'verify' => OptionParser.new do |opts|
-            opts.on('-b', '--begin WORD') do |word|
-              options[:verify_begin] = word
-            end
+              opts.on('-s', '--start LINE', Integer, 'Start with given line') do |line|
+                options[:dictate_start] = line
+              end
 
-            opts.on('-e', '--end WORD') do |word|
-              options[:verify_end] = word
-            end
+              opts.on('-f', '--finish LINE', Integer, 'Finish with given line') do |line|
+                options[:dictate_finish] = line
+              end
+            end,
 
-            opts.on('-s', '--start LINE', Integer) do |line|
-              options[:verify_start] = line
-            end
+            'verify' => OptionParser.new do |opts|
+              opts.banner = set_banner('verify')
 
-            opts.on('-f', '--finish LINE', Integer) do |line|
-              options[:verify_finish] = line
+              opts.on('-b', '--begin WORD', 'Begin with given word') do |word|
+                options[:verify_begin] = word
+              end
+
+              opts.on('-e', '--end WORD', 'End with given word') do |word|
+                options[:verify_end] = word
+              end
+
+              opts.on('-s', '--start LINE', Integer, 'Start with given line') do |line|
+                options[:verify_start] = line
+              end
+
+              opts.on('-f', '--finish LINE', Integer, 'Finish with given line') do |line|
+                options[:verify_finish] = line
+              end
             end
-          end
-      }
-      commands[args.shift].order!
-      options
+        }
+        commands[args.shift].order!
+        options
+      end
+
+      def get_caller_file_name
+        caller[-1].match(/\S+\.rb/)
+      end
+
+      def set_banner(cmd)
+        "Usage: #{get_caller_file_name} #{cmd} [options]"
+      end
     end
 
-    private_class_method :new
+    private_class_method :new, :get_caller_file_name, :set_banner
   end
 end
