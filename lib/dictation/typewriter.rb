e1270f161c48e62c94b0053440c009ebef20b182
Jing Li <thyrlian@gmail.com>
2013-11-21 08:28:10 +0100

    Added basic functions for Typewriter

--- lib/dictation/typewriter.rb
+++ lib/dictation/typewriter.rb
@@ -1,5 +1,16 @@
 module Dictation
   class Typewriter
+    def record
+      print 'Word:        '
+      value = gets.chomp
+      print 'Translation: '
+      translation = gets.chomp
+      Word.new(value, translation)
+    end
 
+    def collect
+      puts 'Please type word and translation one by one.'
+      # loop until user finishes
+    end
   end
 end
