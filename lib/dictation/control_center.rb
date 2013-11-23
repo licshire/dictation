e63f4cc951e857f48d0e4672447f7d3fd7fa1e95
Jing Li <thyrlian@gmail.com>
2013-11-23 21:12:20 +0100

    Added last two execution commands for dictate and verify.  大功告成!

--- lib/dictation/control_center.rb
+++ lib/dictation/control_center.rb
@@ -30,11 +30,7 @@ module Dictation
 
       def execute_add(options)
         if options[:language]
-          regex = /dict_#{options[:language]}_.*\.txt/
-          dictionary = nil
-          Dir.glob("*").each do |file|
-            (dictionary = file) && break if file.match(regex)
-          end
+          dictionary = get_dictionary(options[:language])
           if dictionary
             words = Typewriter.new.collect
             Serializer.serialize(words, dictionary)
@@ -47,14 +43,75 @@ module Dictation
       end
 
       def execute_dictate(options)
-
+        execute_speak(options, :dictates)
       end
 
       def execute_verify(options)
+        execute_speak(options, :verifies)
+      end
 
+      def get_dictionary(language)
+        dictionary = nil
+        regex = /dict_#{language}_[a-z]{2}\.txt/
+        Dir.glob("*").each do |file|
+          (dictionary = file) && break if file.match(regex)
+        end
+        dictionary
+      end
+
+      def get_languages(dictionary)
+        regex = /dict_([a-z]{2})_([a-z]{2})\.txt/
+        md = dictionary.match(regex)
+        [md[1], md[2]]
+      end
+
+      def get_words(options)
+        words = []
+        dictionary = nil
+        language = options[:language]
+        word_begin = options[:word_begin]
+        word_end = options[:word_end]
+        line_start = options[:line_start]
+        line_finish = options[:line_finish]
+
+        if language
+          dictionary = get_dictionary(language)
+        end
+
+        if dictionary && (word_begin || line_start)
+          if word_end
+            words = Serializer.deserialize_a_portion_by_given_word(dictionary, word_begin, word_end)
+          elsif line_finish
+            words = Serializer.deserialize_a_portion_by_given_line(dictionary, line_start, line_finish)
+          else
+            words = (word_begin ? Serializer.deserialize_a_portion_by_given_word(dictionary, word_begin) : Serializer.deserialize_a_portion_by_given_line(dictionary, line_start))
+          end
+        else
+          words = Serializer.deserialize(dictionary)
+        end
+
+        words
+      end
+
+      def set_teacher(dictionary)
+        languages = get_languages(dictionary)
+        tts_dictate = TTS.new(languages[0].intern)
+        tts_verify = TTS.new(languages[1].intern)
+        Teacher.new(tts_dictate, tts_verify)
+      end
+
+      def execute_speak(options, action)
+        words = get_words(options)
+        dictionary = get_dictionary(options[:language])
+        if dictionary
+          teacher = set_teacher(dictionary)
+          teacher.send(action, words)
+        else
+          puts 'Please check if the given language is correct (two-letter country code) or whether the dictionary file exists.'
+        end
       end
     end
 
-    private_class_method :new, :execute_new, :execute_add, :execute_dictate, :execute_verify
+    private_class_method :new, :get_dictionary, :get_languages, :get_words, :set_teacher, :execute_speak, :execute_new, :execute_add, :execute_dictate, :execute_verify
   end
 end
