5fa896dacbe7556c31dbc581f2f1295e5a52d2c7
Jing Li <thyrlian@gmail.com>
2013-11-23 09:34:39 +0100

    Finished collecting input.

--- lib/dictation/typewriter.rb
+++ lib/dictation/typewriter.rb
@@ -1,16 +1,61 @@
 module Dictation
   class Typewriter
+    attr_accessor :words
+
+    def initialize
+      @words = []
+    end
+
+    def get_input_if_not_nil
+      input = gets
+      input.nil? ? nil : input.chomp!
+    end
+
     def record
-      print 'Word:        '
-      value = gets.chomp
-      print 'Translation: '
-      translation = gets.chomp
-      Word.new(value, translation)
+      paint_print('Word:         ', :blue)
+      value = get_input_if_not_nil
+      paint_print('Translation:  ', :yellow)
+      translation = get_input_if_not_nil
+      @words.push(Word.new(value, translation)) if value && translation
     end
 
     def collect
-      puts 'Please type word and translation one by one.'
-      # loop until user finishes
+      paint_puts('Please type word and translation one by one, press Enter key to save.  When you finish, press Ctrl+C.', :pink)
+      Signal.trap('SIGINT') do
+        puts ''
+        break
+      end
+      loop do
+        record
+      end
+      paint_puts("=" * 80, :green)
+      paint_puts("Input is done.", :green)
     end
+
+    def paint(text, color)
+      case color
+        when :green
+          color_code = 32
+        when :yellow
+          color_code = 33
+        when :blue
+          color_code = 34
+        when :pink
+          color_code = 35
+        else
+          color_code = 0
+      end
+      "\e[#{color_code}m#{text}\e[0m"
+    end
+
+    def paint_print(text, color)
+      print(paint(text, color))
+    end
+
+    def paint_puts(text, color)
+      puts(paint(text, color))
+    end
+
+    private :get_input_if_not_nil, :record, :paint, :paint_print, :paint_puts
   end
 end
